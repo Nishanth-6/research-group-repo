708494ec04271c1ad92981b6fd849a1e96122bac
Nishanth-6 <nishanthnagendran7@gmail.com>
2026-02-17 19:10:43 -0600

    Added project id

--- src/lib/sanity.ts
+++ src/lib/sanity.ts
@@ -1,8 +1,11 @@
 import { createClient } from '@sanity/client';
 
+const projectId = import.meta.env.VITE_SANITY_PROJECT_ID || 'lyz6if75';
+const dataset = import.meta.env.VITE_SANITY_DATASET || 'production';
+
 export const sanityClient = createClient({
-  projectId: import.meta.env.VITE_SANITY_PROJECT_ID,
-  dataset: import.meta.env.VITE_SANITY_DATASET,
+  projectId,
+  dataset,
   apiVersion: '2024-01-01',
   useCdn: true, // CDN for fast, cached reads
 });
